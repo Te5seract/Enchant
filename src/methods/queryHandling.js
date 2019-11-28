8f3b8d3754c83a9097043cec6dbaa6335acd81c7
Te5seract <isaacastley@live.com>
2019-11-28 19:54:00 +1100

    Swap method update
    Making the swap method accept more than one single query. It can accept any number of them if they're separated by a space or a comma or both.

--- src/methods/queryHandling.js
+++ src/methods/queryHandling.js
@@ -9,11 +9,23 @@ window.__queryHandling__ = (function () {
         proto.swap = function (mixElem) {
             var replaceNode = [];
 
+            // split up the selectors and turn it into an array 
+            mixElem = mixElem.replace(/,| /g, " ").replace(/{| {/g, "~{").split(" ").filter(Boolean);
+
+            en.forEach(mixElem, (i) => {
+                mixElem[i.index] = mixElem[i.index].replace("~", " ");
+
+                // push individual elements into replaceNode array
+                en.forEach(en.selector(mixElem[i.index]), (x) => {
+                    replaceNode.push(en.selector(mixElem[i.index])[x.index]);
+                });
+            });
+
             //  completely clear the selector
             this.length = en.clearSelector(this);
             
             // reset selector size
-            this.length = en.resetSelector(this, en.selector(mixElem));
+            this.length = en.resetSelector(this, en.selector(replaceNode));
 
             return this;
         }; // swap method end
